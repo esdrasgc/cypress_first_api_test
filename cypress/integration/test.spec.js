82cd326b42726c014aee2195f94e72b2937afb1d
Esdras Gomes Carvalho <esdrasgcarvalho@gmail.com>
2021-10-23 14:30:16 -0300

    Created the test to see if cypress is working and all the downloads went correctly

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,14 @@
+// test.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Mocha’s interface', () => {
+    context('it provides a way to keep tests easier to read and organized.', () => {
+      it('This is your test case', () => {
+        expect(true).to.eq(true)
+      });
+    });
+  });
